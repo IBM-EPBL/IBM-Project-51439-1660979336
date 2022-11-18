43cdd40337146f1f268c87acc30b5d85e5d6ff2b
vignesh01vicky <111695626+vignesh01vicky@users.noreply.github.com>
2022-11-18 16:45:37 +0530

    Add files via upload

--- ASSIGNMENTS/TEAM MEMBER-ESAKKI RAJA/Assignment-2/app.py
+++ ASSIGNMENTS/TEAM MEMBER-ESAKKI RAJA/Assignment-2/app.py
@@ -1,62 +1,80 @@
-from flask import Flask, render_template, request, flash
-import pandas as pd
-import csv
+from flask import Flask, render_template, request, redirect, url_for, session
+import re
+
+import ibm_db
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=21fecfd8-47b7-4937-840d-d791d0218660.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31864;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=mqm10930;PWD=0W28Fba9aWlL3Gtd",'','')
 
 app = Flask(__name__)
-
+app.secret_key = 'as'
 @app.route("/")
-def home():
-    return render_template("home.html")
+def Home():
+    return render_template("Home.html")
+@app.route('/' methods=['GET','POST'])
+@app.route('/register',methods=['GET','POST'])
+def Register():
+    msg=""
+    if request.method == 'POST':
+        username=request.form['username']
+        address=request.form['address']
+        email=request.form['email']
+        phone=request.form['phone']
+        password=request.form['password']
+        sql= "SELECT * FROM registers WHERE username=?;"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
 
-@app.route('/login', methods=["GET","POST"])
-def login():
-    if request.method =="POST":
-        username2 =request.form.get("username")
-        password2 = request.form.get("password")
-
-        if username2 == password2 == "admin":
-            flash("Login Success","success")
-            return render_template("register.html")
+    if account:
+        msg="User already exists"
+      return render_template('Register.html', msg=msg)
     else:
-        flash("Invalid Login","danger")
-        return render_template("login.html")
-    return render_template("login.html")
+      insert_sql = "INSERT INTO registers VALUES (?,?,?,?,?)"
+      prep_stmt = ibm_db.prepare(conn, insert_sql)
+      ibm_db.bind_param(prep_stmt, 2, username)
+      ibm_db.bind_param(prep_stmt, 3, address)
+      ibm_db.bind_param(prep_stmt, 4, email)
+      ibm_db.bind_param(prep_stmt, 5, phone)
+      ibm_db.bind_param(prep_stmt, 6, password)
+      ibm_db.execute(prep_stmt)
+      msg='You have successfully registered'
+     return render_template('Login.html', msg="msg")
+else:
+    msg='Please fill out the form'
+    return render_template('Register.html',msg=msg)       
 
-@app.route("/register")
-def register():
-    return render_template("register.html")
-
-@app.route("/about")
-def contact():
-    return render_template("about.html")
-
-@app.route("/register", methods=["GET", "POST"])
-def reg():
+@app.route('/Login', methods=['GET', 'POST'])
+def Login():
+    global userid
+    msg = ' '
     if request.method == "POST":
-        firstname2 = request.form.get("FirstName")
-        lastname2 = request.form.get("LastName")
-        email2 = request.form.get("Email")
-        contact2 = request.form.get("Contact")
-        password2 = request.form.get("Password")
+        username = request.form['username']
+        password = request.form['password']
+        query = "select * from user1 where username=? and password=?"
+        stmt = ibm_db.prepare(connection, query)
+        ibm_db.bind_param(stmt, 1, username)
+        ibm_db.bind_param(stmt, 2, password)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+        if account:
+            session['Loggedin'] = True
+            session['id'] = account['USERNAME']
+            session['username'] = account['USERNAME']
+            msg = 'Logged in Successfully'
+            return render_template('About.html', msg=msg, username=str.upper(username))
+        else:
+            msg = 'Incorrect Username or Password'
+            return render_template('Login.html', msg=msg)
+    else:
+        msg = 'PLEASE FILL OUT OF THE FORM'
+        return render_template('Login.html', msg=msg)
 
-        filename = "static/data.csv"
-        header = ("FirstName", "LastName", "Email", "Contact", "Password")
-        data = [(firstname2, lastname2, email2, contact2, password2)]
-
-        with open(filename, "w", newline="") as csvfile:
-            d = csv.writer(csvfile)
-            d.writerow(header)
-            for x in data:
-                d.writerow(x)
-    
-
-        return "Data has been Stored Successfully"
-
-    return render_template("home.html")
-
-if __name__ == "__main__":
-    app.secret_key = "12345"
-    app.run(debug= True)
 
     
+@app.route("/About")
+def About():
+    return render_template("About.html")
 
+if __name__ == "__main__" :
+    app.run(debug=True)
